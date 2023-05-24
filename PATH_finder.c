33879eef069f79b6637f5e83ad2d37dd178c62b7
moulineE <elmahdi.mouline@live.fr>
2023-05-24 15:32:45 +0000

    completing pathfinder

--- PATH_finder.c
+++ PATH_finder.c
@@ -2,6 +2,8 @@
 
 char *get_path(void);
 char *_getenv(const char *name);
+char *find_path(char *cmd);
+char *add_end(char *dest , const char *cmd);
 
 /**
  * get_path - Returns the value of the PATH enviroment variable.
@@ -32,4 +34,78 @@ char *_getenv(const char *name)
 		}
 	}
 	return (NULL);
+}
+
+/**
+ * find_path - find the path of an exe
+ * @cmd: the exe to find
+ *
+ * Return: the path of cmd
+ */
+char *find_path(char *cmd)
+{
+	char *path, **dir, buffer[4096], *cmd_path;
+	int stat_ret, i = 0, j = 0;
+	struct stat st;
+
+	path = get_path();
+	if (path == NULL)
+	{
+		return (NULL);
+	}
+	dir = _strtok(path, ":");
+	if (dir == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; dir[i]; i++)
+	{
+		for (j = 0; j < 4096; j++)
+		{
+			buffer[j] = 0;
+			_strcpy(buffer, dir[i]);
+			add_end(buffer, cmd);
+			stat_ret = stat(buffer, &st);
+			if (stat_ret == 0)
+			{
+				free_tokens(dir);
+				cmd_path = malloc(sizeof(char) * (_strlen(buffer) + 1 ));
+				if (cmd_path == NULL)
+				{
+					return (NULL);
+				}
+				_strcpy(cmd_path, buffer);
+				return (cmd_path);
+			}
+		}
+	}
+	if (stat_ret == -1)
+	{
+		free_tokens(dir);
+	}
+	return(NULL);
+}
+
+/**
+ * add_end - add the cmd exe at the end of the path
+ * @dest: the path
+ * @cmd: the exe
+ *
+ * Return the exe with it path
+ */
+char *add_end(char *dest , const char *cmd)
+{
+	char *ret = dest;
+
+	while (*dest)
+	{
+		dest++;
+	}
+	*dest++ = '/';
+	while(*cmd)
+	{
+		*dest++ = *cmd++;
+	}
+	*dest = '\0';
+	 return (ret);
 }
